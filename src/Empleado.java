0d65068b3285776d173fdee47dc8e8f9e84fffb2
Luxyz <elkin.diaz@epn.edu.ec>
2024-10-23 22:41:19 -0500

    Commit 1

--- src/Empleado.java
+++ src/Empleado.java
@@ -1,2 +1,47 @@
-package PACKAGE_NAME;public class Empleado {
+public class Empleado {
+    private String nombre;
+    private int edad;
+    private double salario;
+
+    public Empleado(){
+        nombre= "Elkin";
+        edad= 20;
+        salario = 1600.7;
+
+    }
+    public Empleado(String nombre, int edad, double salario){
+        this.nombre=nombre;
+        this.edad= edad;
+        this.salario = salario;
+
+    }
+
+
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public void setNombre(String nombre){
+        this.nombre=nombre;
+    }
+
+    public int getEdad(){
+        return edad;
+    }
+
+    public void setEdad(int edad){
+        this.edad=edad;
+    }
+
+    public double getSalario(){
+        return salario;
+    }
+
+    public void setSalario(double salario){
+        this.salario= salario;
+    }
+
+
 }
+
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,29 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Empleado e1 = new Empleado();
+
+
+        System.out.println("Información del Empleado 1");
+        System.out.println("Nombre: "+e1.getNombre()+"\nEdad: "+e1.getEdad()+"\nSalario: "+e1.getSalario());
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ingrese la siguiente información ");
+        System.out.print("Ingrese el nombre del empleado 2: ");
+        String nombre = sc.nextLine();
+
+        System.out.print("Ingrese la edad del empleado 2: ");
+        int edad = sc.nextInt();
+
+        System.out.print("Ingrese el salario del empleado 2: ");
+        double salario = sc.nextDouble();
+
+        Empleado e2 = new Empleado(nombre, edad, salario);
+        System.out.println("Información del Empleado 2");
+        System.out.println("Nombre: "+e2.getNombre()+"\nEdad: "+e2.getEdad()+"\nSalario: "+e2.getSalario());
     }
+
 }
